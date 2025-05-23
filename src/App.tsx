ffec3dfe7e8605c10efaa4171bf7b1da2cefea9c
Haya <haya.ehab.dev@gmail.com>
2025-05-23 17:55:32 +0300

    edit in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './pages/Home'
@@ -21,7 +21,7 @@ function App() {
         <Toaster position="top-right" />
         <Router>
           <div className="min-h-screen bg-bakery-cream">
-            <Header isCartOpen={isCartOpen} setIsCartOpen={setIsCartOpen} />
+            <Header />
             <main className="container mx-auto px-4 py-8">
               <Routes>
                 <Route path="/" element={<Home />} />
